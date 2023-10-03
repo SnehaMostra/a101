a3850cff831da07ccad32688577c7c464bcd08a8
SnehaMostra <75886121+SnehaMostra@users.noreply.github.com>
2023-10-03 17:51:48 +0530

    Add files via upload

---
+++ a102.py
@@ -0,0 +1,20 @@
+import os, shutil
+src = "C:/Users/skmos/Desktop/C102_assets-main"
+dst = "C:/Users/skmos/Desktop"
+exts = [".txt",".doc",".docx",".pdf"]
+files = os.listdir(src)
+#print(files)
+for file in files:
+    name,ext= os.path.splitext(file)
+    print(ext)
+    if ext=='':
+        continue
+    elif ext in exts:
+        path1 = src+"/"+file
+        path2 = dst+"/documents"
+        path3 = path2+"/"+file
+        if os.path.exists(path2):
+            shutil.move(path1,path3)
+        else:
+            os.mkdir(path2)
+            shutil.move(path1,path3)
